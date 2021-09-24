c474af285d6bf9b850a0889c7bc20ed3c5202591
Adil Omary <adilomary@hotmail.fr>
2021-09-24 15:20:42 +0200

    Adding methods on Shortener model

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -1,17 +1,32 @@
 require 'digest/sha2'
 
 class Shortener
-  attr_reader :url
+  attr_reader :url, :link_model
 
-  def initialize(url)
+  def initialize(url, link_model = Link)
     @url = url
+    @link_model = link_model
   end
 
   def generate_short_link
-    Link.create(original_url: url, lookup_code: lookup_code)
+    link_model.create(original_url: url, lookup_code: lookup_code)
   end
 
   def lookup_code
-    Digest::SHA256.hexdigest(url)[0..6]
+    # get a lookupcode
+    # check if it already exists
+    # break the loop if it does not exist
+    # it not, get an other code
+
+    i = 0
+    loop do
+      code = get_new_code(i)
+      break code unless link_model.exists?(lookup_code: code)
+      i += 1
+    end
+  end
+
+  def get_new_code()
+    Digest::SHA256.hexdigest(url)[i..(i + 6)]
   end
 end
