ff99885fe054d54d977979f05518276838a0bb03
Adil Omary <adilomary@hotmail.fr>
2021-09-29 17:38:48 +0200

    Redirection from shortened url to original url is working

--- spec/requests/link_redirection_spec.rb
+++ spec/requests/link_redirection_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Link redirection', type: :request do
-  it 'redirects to the original URL for a given short link'
+  it 'redirects to the original URL for a given short link' do
     url = 'https://twitter.com/'
     shortener = Shortener.new(url)
     link = shortener.generate_short_link
+
+    get link.shortened_url
+
+    expect(response).to redirect_to(link.original_url)
+  end
+
 end
